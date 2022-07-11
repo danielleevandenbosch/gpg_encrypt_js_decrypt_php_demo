73a8988a10f6830169697fe8b7db594b40a2730b
Daniel Van Den Bosch <danv@vandataconsulting.com>
2022-07-11 16:12:34 -0400

    initial commit this test worked for a client

---
+++ index.php
@@ -0,0 +1,112 @@
+<?php
+$pk = "-----BEGIN RSA PRIVATE KEY-----
+MIIJKQIBAAKCAgEAr9x2jwMlXo3nJlwovHZRrp9/OlU+ZffF1/JEoIgqHHSELzX4
+DJMsnCA8HYYcwCkwX5tiZOuntWKJkv5oRkzexVEsHyQ/TfMSe7Qq4IE2pjBcb3Ct
+KDb15z5lb+VOgtB9OR42FyLU+H05W/I06jMZ4DthZRN2lWBclLNt8vE5Lo5YVNCT
+W+gBmcrGHr2R6/sc0Iw5gDEIG4MZguuc7FEKJqkQ5/V27hfUlE7ONBKpODVGry2f
+ogMe50BhnSueTcg0d0lR1fdDTfkz8LOebWOmDM0PtRthpaP6DpDiLXhAwSsV6sKV
+E+V2EFswF5q619sjUmQ0N4kRlcXxqaRnT0PYY0Vy0a/ZoJ1w0tAumbeAw4HZLj/o
+mUHkqMG3+3wiiy7oulU0he5T0264pMhQhwXTZVqndEIe2txTEXdegMsL6U3vXQac
+6HtPZTMdJnxI9x+oaGaIvv8D7bjjZYhdGgNLW2u651aIXOS0Z0HGVSnrVIyAew6O
+3rdoObrrhC/y9aw59+iOrDTI5pZHgwDYa8H96iCy/7nEw1VbVx5O67Ayyr2tWMD8
+pIr0y/9yDuvUHF0XprJFkg8jGGSBafIWsyNzbtrFZ9oC0ItzOIoHxDgIHLB9QxCb
+veVXTA6i9M/0DXvWJ2tdhjkLO3s/g8CJ8q8eaqOcogQSxJIwRGtgPU9OptkCAwEA
+AQKCAgEAisFbNhmYWZODJwweBB0i87Ha067AjfnCO1Fm/naRK2Rvi0R804cn8oOo
+iu7vLlsRVM4oA1LAP2hg/f8HTTiHqI4UTvntjNZV5g7QqdSpxtRvimoP5aP/v5XG
+uVPm72YK948zxGtyOCGtqTNgtszdBMQ1Y2fI77FZOuabwMy9flNHtJvhOOnEoZCR
+Y13VjajTImURVWjaacK11UuLxBVo+fn5YduWG+PnkaDgJ8v1gmjevYc05cKqlnGA
+017AB8UISHosPLNcgmA2sBs4zDyMnpsJYq8ZC3Vm7ik4nAG+resN3u3qbAlnbx5T
+wrwN4hSAORjg7lSS8GZ+94fFD5CLYO0nBRdwlMJqjsNz7+L6qsKx9DQNUFDrRuo7
+oKoYZ1EONqtvZ1gXhXlu22txbayWR+XFmGVpBVsPtNy5UNl2bvxJ63cpRIEFdQyz
+l32btpZwBFSiSXqSKHJaJBlSnBrwhSRpsjko0mxk6Ckbrx5ElgtvPq41YHTxQxr8
+l99jymvnUNeNSXsPjpveXE3hPXx/VdznVXbkJbfYXpCFkQWaoeujhFZlxbeeCITu
+UIX5tpcQ7J9LPL3+n0Q1/MFTnRTaZQkRr2+leiTlNsjzReEpGE7GJlCNhDXDaTCV
+lGci/96un1cE7YvhzYblN6CFO1euHGtb5Xe3wTlGSiHAoRfluJECggEBAPjqAUb8
+vcKK1jdwbZYG5FnfQ7vY4ErUrr9AmUCShwGLNsrOhHIde3SQF+bM3UsJHttYAyg0
+xwn8uywv/+s8FaSIdcRESOIQYOLMhX3GjXEUvJC9m15gabwkUfjmhSNC+YW17ivJ
+7+os8MwrUrrc+oOi3hCJ+ObKWvvH9Ywzydl+qzEEDXNf8GaKl2vOU3ffZJ/96YM+
+EEuvVEidtzXTRIk/7sNTkNiik0xq6AF37zVFYJF9piEdA5j/iWd/Ha+4IvsbwRV9
+Y+8JeV/bc8OaH1sltHkJNpeumMIpw0aE4CyllK13fgUH9vsQFkiXB7rcZTqdW/Fc
+T3W8+OoZFu+jNs0CggEBALTeE0SFUNOp6bvvO6MUcP1Tn+/vuAX2GYTSnL9RQNJv
+xMst2GBcHsp5zF+Ky+eicaXneDcpWX5bWP/ndPQxWuD9ZLyAy7Vqks+yBlDeoAfn
+4BKTfyxRI7EqMv7d/coVNFyOiuENHZEnsm7fisTJB4ACgD7rLNPNKikhthPg8kRS
+3Tsfa7JFNCxRL3dTvGeAxJqwJ2p08+co2lkH5oCaWg6s7sQtJ0FTZOZsPqctQUlZ
+uVOkGj5CYCG/qwHLlHCXKUKuArT9sxkvlOJd3hRmQlP/gwiR4teJArqmNgYO7wo7
+mTbYR+ptdHNW2R1deWI1I4Ix8N350/fNWQAoYvGk+D0CggEARF2BHE4ukFAxpehr
+MNmTvltj7yQR9nuTw1Oqw+F60yG5Wtp95hnS3qD075806HcI0pXJyDE2ubeAcRJ9
+584I47fXIKDgo7Z75c7mmzT9hCF96QMquTEh2VrgaMxnpNewUNk3IS/FhTvypyD/
+B/PJmn4RH9nFUx4g/yZcerV+qw3vVvcV+qpPDNrj2+g/ZJ0mtgq3KCTTtPXi4E7x
+EweDu26j1WIOVnHQoxx5vG2Edc0oSM2J425knpEQ3Cw9qxWYBgAJeC2IVQ71Fy7f
+DMo38oDYoKW3KNH7gtiVmbjFGX4g58P6CMe3Ph2dATL6HIiMBR1TM2d3K49LjoB1
+nDvnwQKCAQEArmYIXCTUiBQpjY6jiZ9XR93PtVMHta/UcW+Hty6I+6DMOEIPhvKs
+uf3xpBq4fLT1Mszb7QyQ/7bjPXYYHrJMd++9p8nlIxwNTfnR/z57PsRGGoNguAHR
+Ea6oF3tEkQwbPjgwD8tOvD1z31brfs58zeEbeg/YXK5SvfCEmexojpr8OVsVxILY
+SPdLdpsmawMOED5U82CsIjQRPpHQoCa1ObtkS4Nie6oG++xaCWcosaqzecQdjfUA
+ruoa4ZNEocAQB/y0HYMXo+xuekuarT7hLA+PcL8C5WN2XzWPmA3kTpCjLvAjgBdk
+lyaiGZ+dQKYOhCbVo6SW0DWQYQeQSIMS3QKCAQACHnn5r3ttYlg67XKaMlPZ+W+R
+Dk9P71If0mBtM3HZZS+hJO5ErM5qRmNf7ARm/6fYR/+QmSbXdvsvyqzWwi2Iotnk
+KBbEFp2buP1/RGzWiu/kNuaOmTG80tXloL6P/xNf8xLO1cLpZDVesf+ohVc3Rmej
+5Ic5itvJujcXej2gyH+aN8vnAqeV1In1ZuqC1c3Eh4hYqkhMdL7YeDlYnTX/1jK8
+kkpuBTSwyQIn0qGYFeQvkZpsoJtQv47FARgLil3eMfgg/TRgwAFaPBYwjaW9AUgY
+/E++n63+SXVEIB8psekNcY/PM9IbVwDMfYfPHrFcOaxfb69Krk8G3sIyk6AS
+-----END RSA PRIVATE KEY-----";
+$kh = openssl_pkey_get_private($pk);
+$details = openssl_pkey_get_details($kh);
+
+function to_hex($data)
+{
+    return strtoupper(bin2hex($data));
+}
+
+?>
+
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!--<script src="base64.js"></script>-->
+    <!--<script src="ec.js"></script>-->
+    <!--<script src="jsbn2.js"></script>-->
+    <script src="jsbn.js"></script> <!-- https://stackoverflow.com/questions/12457234/encrypt-in-javascript-decrypt-in-php-using-public-key-cryptography -->
+    <script src="prng4.js"></script>
+    <script src="rng.js"></script>
+    <script src="rsa.js"></script>
+    <!--<script src="sec.js"></script>-->
+    <!--<script src="sha1.js"></script>-->
+    <title>Document</title>
+</head>
+<body>
+    <h1>
+        jsbn demo
+    </h1>
+    <main id="abc"></main>
+</body>
+</html>
+<script>
+    var rsa = new RSAKey();
+    rsa.setPublic('<?php echo to_hex($details['rsa']['n']) ?>', '<?php echo to_hex($details['rsa']['e']) ?>');
+    // encrypt using RSA
+    var data = rsa.encrypt('this guy');
+    document.getElementById("abc").innerHTML = '<a href="decode.php?huge_string='+data+'">' + data + '</a>';
+    // the public key is
+    /*
+    -----BEGIN PUBLIC KEY-----
+MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAr9x2jwMlXo3nJlwovHZR
+rp9/OlU+ZffF1/JEoIgqHHSELzX4DJMsnCA8HYYcwCkwX5tiZOuntWKJkv5oRkze
+xVEsHyQ/TfMSe7Qq4IE2pjBcb3CtKDb15z5lb+VOgtB9OR42FyLU+H05W/I06jMZ
+4DthZRN2lWBclLNt8vE5Lo5YVNCTW+gBmcrGHr2R6/sc0Iw5gDEIG4MZguuc7FEK
+JqkQ5/V27hfUlE7ONBKpODVGry2fogMe50BhnSueTcg0d0lR1fdDTfkz8LOebWOm
+DM0PtRthpaP6DpDiLXhAwSsV6sKVE+V2EFswF5q619sjUmQ0N4kRlcXxqaRnT0PY
+Y0Vy0a/ZoJ1w0tAumbeAw4HZLj/omUHkqMG3+3wiiy7oulU0he5T0264pMhQhwXT
+ZVqndEIe2txTEXdegMsL6U3vXQac6HtPZTMdJnxI9x+oaGaIvv8D7bjjZYhdGgNL
+W2u651aIXOS0Z0HGVSnrVIyAew6O3rdoObrrhC/y9aw59+iOrDTI5pZHgwDYa8H9
+6iCy/7nEw1VbVx5O67Ayyr2tWMD8pIr0y/9yDuvUHF0XprJFkg8jGGSBafIWsyNz
+btrFZ9oC0ItzOIoHxDgIHLB9QxCbveVXTA6i9M/0DXvWJ2tdhjkLO3s/g8CJ8q8e
+aqOcogQSxJIwRGtgPU9OptkCAwEAAQ==
+-----END PUBLIC KEY-----
+     */
+</script>
